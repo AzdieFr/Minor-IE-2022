f1c4ef94f61092fdd0d92244d3da694b2c05b334
Azdie <59054008+AzdieFr@users.noreply.github.com>
2022-09-21 14:18:23 +0200

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,9 @@
+class Colors():
+    RED = (0, 255, 0)
+    ORANGE = (255,140,0)
+    YELLOW = (255,255,0)
+    GREEN = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    VIOLET = (148,0,211)
+    BLACK = (0,0,0)
+    WHITE = (255, 255, 255)
