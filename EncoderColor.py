324c24dc9097fe57c58e3c18fb3fb9c430f9565f
Azdie <59054008+AzdieFr@users.noreply.github.com>
2022-09-22 13:13:43 +0200

    Delete EncoderColor.py

--- EncoderColor.py
+++
@@ -1,7 +0,0 @@
-import neopixel
-from rainbowio import colorwheel
-
-class EncoderColor:
-
-    def __init__(self, pin, pixnum, br):
-        self.pixels = neopixel.NeoPixel(pin, pixnum, brightness = br, auto_write=False, pixel_order=neopixel.RGB)
